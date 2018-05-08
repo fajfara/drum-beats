d3fb85d3575fc972ac6cf154f38be12fb8ed556f
fajfara <a.fajfar95@gmail.com>
2018-05-08 18:37:25 +0200

    Trying something out

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-function test() {
-    alert("IT WORKS!");
+var generatedSeq = [0,1,2,3,0,1,4];
+var firstDrum = document.getElementById("kick-drum"); 
+
+function startGame () {
+    for(let i = 0; i < generatedSeq.length; i++){
+        console.log(generatedSeq[i]);
+        if(generatedSeq[i] == 0){
+            firstDrum.classList.add("animate--beat");
+            firstDrum.classList.remove("animate--beat");
+            
+        }
+    }
+}
+
+window.onload = function() {
+    startGame();
 }
