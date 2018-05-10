21676799803b01137653a6ad4c734a04bb6df1ae
fajfara <a.fajfar95@gmail.com>
2018-05-10 20:00:44 +0200

    Adding the README file, all changes listed in there

---
+++ README.md
@@ -0,0 +1,12 @@
+# Drum beats
+
+This is a project done for FCC, my variation on the Simon says app.
+
+## Changelog
+DATE: 10.5.2018
+> Added the sounds to the drum, a button that starts the sequence of drum playing. Game doesn't have the actual game functionality yet. Bugs i currently have:
+⋅⋅* The button bug, moves to another position when clicked
+⋅⋅* Sound bug, sometimes if the same drum is played right after the sound won't play. Will have to play around the settimeout function.
+⋅⋅* Atm not responsive website, but that will come latter on
+
+Plans: By the end of the month have the core game mechanic done so you can play. Later on add the leaderboards feature so you can save your highscore.
--- js/app.js
+++ js/app.js
@@ -1,29 +1,67 @@
 let userSeq = [];
 let simonSeq = [];
-let count = 20;
+let count = 5;
 let on;
 let off;
 let j = 0;
-let snareDrum = document.getElementById('snare-drum');
-let leftTom = document.getElementById('left-tom');
-let rightTom = document.getElementById('right-tom');
-let kickDrum = document.getElementById('kick-drum');
-let rightCymbal = document.getElementById('right-cymbal');
-let leftCymbal = document.getElementById('left-cymbal-top-one');
+const snareDrum = document.getElementById('snare-drum');
+const leftTom = document.getElementById('left-tom');
+const rightTom = document.getElementById('right-tom');
+const kickDrum = document.getElementById('kick-drum');
+const rightCymbal = document.getElementById('right-cymbal');
+const leftCymbal = document.getElementById('left-cymbal-top-one');
+const overlay = document.getElementById('overlay');
+const drumContainer = document.getElementById('drum-container');
+const startButton = document.getElementById('start-button');
+
+// audio variables
+const snareDrumAudio = new Audio('./drum-sounds/snare.wav');
+const leftTomAudio = new Audio('./drum-sounds/left-tom.wav');
+const rightTomAudio = new Audio('./drum-sounds/right-tom.wav');
+const kickDrumAudio = new Audio('./drum-sounds/kickdrum.wav');
+const rightCymbalAudio = new Audio('./drum-sounds/cymbal.wav');
+const leftCymbalAudio = new Audio('./drum-sounds/closed-cymbal.wav');
 
 window.onload = function() {
-    for(let i = 0; i < 20; i++){
+
+    for(let i = 0; i < 5; i++){
         simonSeq[i] = Math.ceil((Math.random() * 6));
     }
     console.log(simonSeq);
-    startSequence();
+    startScreen();
+
+    startButton.addEventListener("click", function(){
+        startSequence();
+    })
+
 
 
 
 };
 
+function startScreen() {
+    setTimeout(() => {
+        overlay.classList.add('hide');
+        drumContainer.classList.add('fade-in');
+    }, 1800);
+    setTimeout(() => {
+        drumContainer.classList.remove('hide');
+        drumContainer.classList.add('show');
+    }, 1900);
+}
+
+function playAudio(source) {
+    setTimeout(function(){
+        source.play();
+        setTimeout(function(){
+            source.pause();
+            source.currentTime = 0;
+        }, 900)
+    }, 100);
+}
+
 function startSequence() {
-    // set inteveral length
+    // set interval length
     if(count <= 10){
         off = 400;
         on = 800;
@@ -36,43 +74,58 @@ function startSequence() {
     x = setInterval(function () {
         if(simonSeq[j] == 1){
             snareDrum.classList.add("animate--beat");
+            console.log("Inside of snare drum");
+            
+            playAudio(snareDrumAudio);
+            // trying to fix the bug with audio
+            
+
             setTimeout(function() {
                 snareDrum.classList.remove("animate--beat");
-            }, off);
+            }, 200);
         }
         else if(simonSeq[j] == 2) {
             leftTom.classList.add("animate--beat");
+            console.log("Inside of left tom drum");
+            playAudio(leftTomAudio);
             setTimeout(function() {
                 leftTom.classList.remove("animate--beat");
             }, off);
         }
         else if(simonSeq[j] == 3) {
             rightTom.classList.add("animate--beat");
+            console.log("Inside of right tom drum");
+            playAudio(rightTomAudio);
             setTimeout(function() {
                 rightTom.classList.remove("animate--beat");
             }, off);
         }
         else if(simonSeq[j] == 4) {
             kickDrum.classList.add("animate--beat");
+            console.log("Inside of kick drum");
+            playAudio(kickDrumAudio);
             setTimeout(function() {
                 kickDrum.classList.remove("animate--beat");
             }, off);
         }
         else if(simonSeq[j] == 5) {
             rightCymbal.classList.add("cymbal-shake");
+            console.log("Inside of open cymbal");
+            playAudio(rightCymbalAudio);
             setTimeout(function() {
                 rightCymbal.classList.remove("cymbal-shake");
             }, off);
         }
         else if(simonSeq[j] == 6) {
             leftCymbal.classList.add("move-cymbal-down");
+            console.log("Inside of closed cymbal");
+            playAudio(leftCymbalAudio);
             setTimeout(function() {
                 leftCymbal.classList.remove("move-cymbal-down");
             }, off);
         }
         j++;
-        console.log(j);
-        console.log(count);
+        console.log("This is the: " + j + "nth time throught the interval");
         if(j == count){
             clearInterval(x);
         }
